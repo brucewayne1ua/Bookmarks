08c929d1d14c3303d6b2f305f93089acf723b2f7
Rustam Ohli <adamsonrustam@gmail.com>
2024-03-03 05:57:11 +0200

    регистрация пользователей через админку

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
  # path('login/', views.user_login, name='login'),
  # url-адреса входа и выхода
