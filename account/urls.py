923ebdfeecf170b6ab4f98dd27eda9d0086cea91
Rustam Ohli <adamsonrustam@gmail.com>
2024-03-04 01:14:36 +0200

    изменение информации об аккаунте

--- account/urls.py
+++ account/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
 path('', include('django.contrib.auth.urls')),
 path('dashboard/', views.dashboard, name='dashboard'),
 path('register/', views.register, name='register'),
+path('edit/', views.edit, name='edit'),
 
 ]
 
--- account/views.py
+++ account/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import LoginForm, UserRegistrationForm, \
+                   UserEditForm, ProfileEditForm
 from .models import Profile
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -13,21 +16,24 @@ def user_login(request):
             user = authenticate(request,
                                 username=cd['username'],
                                 password=cd['password'])
-        if user is not None:
-            if user.is_active:
-                login(request,user)
-                return HttpResponse('Authenticated successfully')
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
             else:
-                return HttpResponse ('Disabled account')
-        else:
-            return HttpResponse('Invalid login')
+                return HttpResponse('Invalid login')
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
 
+
 @login_required
 def dashboard(request):
-    return render(request,'account/dashboard.html',{'section':'dashboard'})
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
 
 
 def register(request):
@@ -51,6 +57,29 @@ def register(request):
     return render(request,
                   'account/register.html',
                   {'user_form': user_form})
-        
 
 
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                      'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
